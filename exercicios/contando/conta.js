a8a5f1f57cadeec60a5cbb2338fe63d45ef33c2c
Conrado Vinha <conradovinha@gmail.com>
2023-07-06 18:12:22 -0300

    up

---
+++ exercicios/contando/conta.js
@@ -0,0 +1,8 @@
+function contar(){
+    //cirando variaveis
+    let ini = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+}
